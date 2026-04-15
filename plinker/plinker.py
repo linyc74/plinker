6cacd6bafc8eac1c0dfca9d4c4af9c6c4853df89
linyc74 <ylin@nycu.edu.tw>
2026-04-15 14:54:41 +0800

    first commit

---
+++ plinker/plinker.py
@@ -0,0 +1,183 @@
+import shutil
+import pandas as pd
+from os.path import basename, dirname
+from .utils import edit_fpath
+from .template import Processor
+
+
+PLINK_EXE = f'{dirname(dirname(__file__))}\\plink.exe'
+
+
+class Plinker(Processor):
+    
+    bfile: str
+    id_link_xslx: str
+    phenotype_xslx: str
+    uudi_column: str
+    tpmi_id_column: str
+    phenotype_column: str
+    minimum_minor_allele_frequency: float
+    maximum_per_variant_missing_genotype_rate: float
+    maximum_per_sample_missing_genotype_rate: float
+    hardy_weinberg_p_value_threshold: float
+
+    keep_samples_phen: str
+
+    def main(
+            self,
+            bfile: str,
+            id_link_xslx: str,
+            phenotype_xslx: str,
+            uudi_column: str,
+            tpmi_id_column: str,
+            phenotype_column: str,
+            minimum_minor_allele_frequency: float,
+            maximum_per_variant_missing_genotype_rate: float,
+            maximum_per_sample_missing_genotype_rate: float,
+            hardy_weinberg_p_value_threshold: float):
+
+        self.bfile = bfile
+        self.id_link_xslx = id_link_xslx
+        self.phenotype_xslx = phenotype_xslx
+        self.uudi_column = uudi_column
+        self.tpmi_id_column = tpmi_id_column
+        self.phenotype_column = phenotype_column
+        self.minimum_minor_allele_frequency = minimum_minor_allele_frequency
+        self.maximum_per_variant_missing_genotype_rate = maximum_per_variant_missing_genotype_rate
+        self.maximum_per_sample_missing_genotype_rate = maximum_per_sample_missing_genotype_rate
+        self.hardy_weinberg_p_value_threshold = hardy_weinberg_p_value_threshold
+
+        self.build_keep_samples_phen()
+        self.plink_keep()
+        self.plink_pheno()
+        self.plink_qc()
+        self.plink_assoc()
+
+    def build_keep_samples_phen(self):
+        id_link_df = pd.read_excel(self.id_link_xslx)
+        phenotype_df = pd.read_excel(self.phenotype_xslx, usecols=[self.uudi_column, self.phenotype_column])
+        fam_df = read_fam(path=f'{self.bfile}.fam')
+
+        phenotype_df = phenotype_df.merge(
+            right=id_link_df,
+            how='inner',
+            left_on=self.uudi_column,
+            right_on=self.uudi_column,
+        )
+        phenotype_df.drop(columns=[self.uudi_column], inplace=True)
+        
+        fam_df = fam_df.merge(
+            right=phenotype_df,
+            how='inner',
+            left_on='IID',
+            right_on=self.tpmi_id_column,
+        )
+        fam_df['PHENO'] = fam_df[self.phenotype_column]
+        fam_df.drop(columns=[self.tpmi_id_column, self.phenotype_column], inplace=True)
+
+        self.keep_samples_phen = f'{self.workdir}\\keep_samples.phen'
+        fam_df[['FID', 'IID', 'PHENO']].to_csv(self.keep_samples_phen, index=False, header=False, sep=' ')
+
+    def plink_keep(self):
+        out = edit_fpath(
+            fpath=self.bfile,
+            old_suffix='',
+            new_suffix='_keep',
+            dstdir=self.workdir
+        )
+        lines = [
+            f'{PLINK_EXE}',
+            f'--bfile {self.bfile}',
+            f'--keep {self.keep_samples_phen}',
+            '--make-bed',
+            f'--out {out}',
+            f'> {out}.stdout',
+        ]
+        self.call(self.CMD_LINEBREAK.join(lines))
+        self.bfile = out
+
+    def plink_pheno(self):
+        out = edit_fpath(
+            fpath=self.bfile,
+            old_suffix='',
+            new_suffix='_pheno',
+            dstdir=self.workdir
+        )
+        lines = [
+            f'{PLINK_EXE}',
+            f'--bfile {self.bfile}',
+            f'--pheno {self.keep_samples_phen}',
+            f'--mpheno 1',
+            '--make-bed',
+            f'--out {out}',
+            f'> {out}.stdout',
+        ]
+        self.call(self.CMD_LINEBREAK.join(lines))
+        self.bfile = out
+
+    def plink_qc(self):
+        out = edit_fpath(
+            fpath=self.bfile,
+            old_suffix='',
+            new_suffix='_qc',
+            dstdir=self.workdir
+        )
+        lines = [
+            f'{PLINK_EXE}',
+            f'--bfile {self.bfile}',
+            f'--maf {self.minimum_minor_allele_frequency}',
+            f'--geno {self.maximum_per_variant_missing_genotype_rate}',
+            f'--mind {self.maximum_per_sample_missing_genotype_rate}',
+            f'--hwe {self.hardy_weinberg_p_value_threshold}',
+            '--make-bed',
+            f'--out {out}',
+            f'> {out}.stdout',
+        ]
+        self.call(self.CMD_LINEBREAK.join(lines))
+        self.bfile = out
+    
+    def plink_assoc(self):
+        out = edit_fpath(
+            fpath=self.bfile,
+            old_suffix='',
+            new_suffix='_assoc',
+            dstdir=self.workdir
+        )
+        lines = [
+            f'{PLINK_EXE}',
+            f'--bfile {self.bfile}',
+            '--assoc',
+            '--adjust',
+            f'--out {out}',
+            f'> {out}.stdout',
+        ]
+        self.call(self.CMD_LINEBREAK.join(lines))
+
+        for src_ext, dst_ext in [
+            ('.assoc', '.txt'),
+            ('.assoc.adjusted', '-adjusted.txt'),
+            ('.log', '.log'),
+            ('.stdout', '.stdout'),
+        ]:
+            src = f'{out}{src_ext}'
+            dst = f'{self.outdir}\\association{dst_ext}'
+            shutil.copy(src, dst)
+
+
+def read_fam(path: str) -> pd.DataFrame:
+    # '\t' or ' ' could be used as separator, determine it by the first line
+    with open(path) as fh:
+        first_line = fh.readline()
+        if '\t' in first_line:
+            sep = '\t'
+        else:
+            sep = ' '
+    df = pd.read_csv(path, sep=sep, header=None)
+    df.columns = ['FID', 'IID', 'PID', 'MID', 'SEX', 'PHENO']
+    return df
+
+
+def read_bim(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path, sep='\t', header=None)
+    df.columns = ['CHR', 'SNP', 'CM', 'BP', 'A1', 'A2']
+    return df
---
+++ test/test_plinker.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from os.path import exists
+from .setup import TestCase
+from plinker.plinker import Plinker, read_fam, read_bim
+
+
+class TestPlinker(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    # def tearDown(self):
+    #     self.tear_down()
+
+    def test_main(self):
+        Plinker(settings=self.settings).main(
+            bfile=f'{self.indir}/1kg_phase1_all',
+            id_link_xslx=f'{self.indir}/1kg_phase1_all_ID_link.xlsx',
+            phenotype_xslx=f'{self.indir}/1kg_phase1_all_phenotype.xlsx',
+            uudi_column='uuid',
+            tpmi_id_column='TPMINUM',
+            phenotype_column='PHENOTYPE_A',
+            minimum_minor_allele_frequency=0.01,
+            maximum_per_variant_missing_genotype_rate=0.05,
+            maximum_per_sample_missing_genotype_rate=0.05,
+            hardy_weinberg_p_value_threshold=0.001,
+        )
+
+
+class TestFunctions(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    def tearDown(self):
+        self.tear_down()
+
+    def test_read_fam(self):
+        df = read_fam(path=f'{self.indir}/wgas1.fam')
+        self.assertEqual(df.shape, (90, 6))
+    
+    def test_read_bim(self):
+        df = read_bim(path=f'{self.indir}/wgas1.bim')
+        self.assertEqual(df.shape, (228694, 6))
+
